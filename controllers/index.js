6c111af4c787c8b547cc46bd082303d4d95c0626
alexberes <aalexbberes@gmail.com>
2021-11-15 09:19:00 -0800

    added routes and models

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const apiRoutes = require("./api")
 router.use("/api",apiRoutes)
+router.use("/exercise",(req,res)=>{
+    res.sendFile(path.join(__dirname, '../public/exercise.html'))
+})
+router.use("/stats",(req,res)=>{
+    res.sendFile(path.join(__dirname, 'stats.html'))
+})
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const routes = require('./controllers');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,9 +16,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.get("/exercise",(req,res)=>{
+  res.sendFile(path.join(__dirname, './public/exercise.html'))
+})
+app.get("/stats",(req,res)=>{
+  res.sendFile(path.join(__dirname, './public/stats.html'))
+})
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 
 app.listen(PORT, () => {
